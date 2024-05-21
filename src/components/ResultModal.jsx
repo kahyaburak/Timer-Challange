ec8c90058fb0bf8db7618b8b8e441f95f9a05e02
Burak <burakkahyabk@gmail.com>
2024-05-21 21:43:20 +0300

    switched the logic to be based on the Remaining Time

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,12 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+const ResultModal = forwardRef(function ResultModal(
+  {remainingTime, targetTime, onReset},
+  ref
+) {
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   const dialog = useRef();
 
   useImperativeHandle(ref, () => {
@@ -13,14 +19,15 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} </strong> seconds.
       </p>
       <p>
-        You stopped the timer with <strong>X </strong> seconds left.
+        You stopped the timer with <strong>{formattedRemainingTime} </strong>
+        seconds left.
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
